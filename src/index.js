0d629767c2224e1401d9d704afedfc800d6c6085
RomanSamohwal <https://github.com/RomanSamohwal>
2021-01-25 01:49:44 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import './scss/index.scss'
+import './scss/index.scss';
+import './module';
 
-console.log('Working!')
+console.log('Working!!!!!!');
---
+++ src/module.js
@@ -0,0 +1,6 @@
+
+// eslint-disable-next-line require-jsdoc
+async function start() {
+  return await Promise.resolve('work async')
+}
+start().then(console.log)
