07c16c0eb9c7a1c329ef7dab68eb9583a3739eee
RomanSamohwal <https://github.com/RomanSamohwal>
2021-02-02 16:12:35 +0300

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,21 +3,29 @@ const CODES = {
   Z: 90
 }
 
-// eslint-disable-next-line no-unused-vars
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class = 'cell' contenteditable></div>
+    <div class = 'cell' contenteditable data-col = '${col}'></div>
   `
 }
 
-function toColumn(col) {
-  return `<div class='column'> ${col} </div>`
+function toColumn(col, index) {
+  return `
+  <div class='column' data-type = 'resizable' data-col = '${index}'>
+    ${col} 
+    <div class='col-resize' data-resize = 'col'></div>
+  </div>`
 }
 
 function createRow(index, content) {
+  const resize = index
+      ? `<div class='row-resize' data-resize = 'row'></div>`
+      : ''
   return `
-  <div class='row'>
-    <div class='row-info'>${index ? index : ''}</div>
+  <div class='row' data-type ='resizable'>
+    <div class='row-info'>${index ? index : ''}
+            ${resize}
+    </div>
     <div class='row-data'>${content}</div>
   </div>`
 }
@@ -44,6 +52,5 @@ export function createTable(rowsCounts = 15) {
         .join('')
     rows.push(createRow(i + 1, cells))
   }
-
   return rows.join('')
 }
